5926cbdfda10492ae69920bc3e17d56cf4c3da1e
RAM KHANDEKAR <spareid200612@gmail.com>
2025-09-20 13:55:03 +0530

    Update webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,23 +1,31 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: {
-    main: './src/index.js',
-  },
+  entry: { main: './src/index.js' },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public/dist'),
+    clean: true,
   },
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: { filename: 'assets/[hash][ext][query]' },
       },
     ],
   },
+  resolve: { extensions: ['.js', '.json'] },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'public/*.html', to: '[name].[ext]' },
+        { from: 'public/**/*.css', to: '[name].[ext]' },
+        { from: 'public/**/*.js', to: '[name].[ext]' },
+      ],
+    }),
+  ],
 };
